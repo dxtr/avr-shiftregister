43d82fb3e97de84a0d01a17c205ad4f171b48599
Kim Lidström <kim@dxtr.im>
2014-01-21 21:20:50 +0100

    Moved delay_ms to misc.{c,h}

---
+++ src/misc.c
@@ -0,0 +1,9 @@
+#include <stdint.h>
+
+#include "misc.h"
+
+/*
+ * delays for a certain time
+ * Doesn't currently work for delay > 4600
+ */
+void delay_ms(uint16_t delay);
---
+++ src/misc.h
@@ -0,0 +1,8 @@
+#ifndef MISC_H_
+#define MISC_H_
+
+#include <stdint.h>
+
+void delay_ms(uint16_t delay);
+
+#endif /* MISC_H_ */
